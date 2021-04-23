f3d8d57f26bbed833255888709a7458ad4861a31
Eri Schon <erischon@gmail.com>
2021-04-23 18:20:58 +0200

    Update travis.py

--- core/settings/travis.py
+++ core/settings/travis.py
@@ -1,5 +1,10 @@
 from . import *
 
+SECRET_KEY = os.environ.get("SECRET_KEY", "password123")
+DEBUG = True
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '[::1]']
+
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
