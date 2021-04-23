cfb57c57472ff92dde5391e25217b33fd06a0188
Eri Schon <erischon@gmail.com>
2021-04-23 10:52:43 +0200

    Update settings.py

--- core/settings.py
+++ core/settings.py
@@ -102,20 +102,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
 
 SECRET_KEY = os.environ.get("SECRET_KEY", "toto")
 DEBUG = int(os.environ.get("DEBUG", default=0))
-# ALLOWED_HOSTS = ['46.101.141.242', '0.0.0.0', 'localhost', '127.0.0.1', '[::1]']
 ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS").split(" ")
-
-
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#         'NAME': 'thesubstitute', 
-#         'USER': 'erischon', 
-#         'PASSWORD': 'Thes2021=',
-#         'HOST': 'localhost', 
-#         'PORT': '',
-#     }
-# }
 
 DATABASES = {
     "default": {
