b6798383eda34647e00b32c482882642f5b08579
Eri Schon <erischon@gmail.com>
2021-04-26 15:22:20 +0200

    Create cron.sh

---
+++ cron.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+export DJANGO_SETTINGS_MODULE="core.settings.production"
+export SECRET_KEY="password123"
+export DB_NAME="thesubstitute"
+export DB_USER="erischon"
+export DB_PASSWORD="Thes2021="
+/home/home/erischon/p10_digitalocean/venv/bin/python3 /home/erischon/p10_digitalocean/manage.py etl
