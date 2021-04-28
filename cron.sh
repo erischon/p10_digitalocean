4be143e83df37b310cc18e0ce6a5ff53a4d0ee5d
Eri Schon <erischon@gmail.com>
2021-04-28 10:33:10 +0200

    cron ok

--- cron.sh
+++ cron.sh
@@ -5,6 +5,8 @@ export SECRET_KEY="password123"
 export DB_NAME="thesubstitute"
 export DB_USER="erischon"
 export DB_PASSWORD="Thes2021="
-/home/home/erischon/p10_digitalocean/venv/bin/python3 /home/erischon/p10_digitalocean/manage.py etl
+
+cd /home/erischon/p10_digitalocean/
+/home/erischon/p10_digitalocean/venv/bin/python3 /home/erischon/p10_digitalocean/manage.py etl
 
 echo "Hello World!" > /home/erischon/test.log
